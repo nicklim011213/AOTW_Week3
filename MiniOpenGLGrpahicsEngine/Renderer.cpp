2adc782303a1410b0df2bde249ae5f19c9820cbb
nicklim011213 <nicklim011213@gmail.com>
2025-01-28 23:51:42 -0500

    Add GLM finish 3d Rendering

--- MiniOpenGLGrpahicsEngine/Renderer.cpp
+++ MiniOpenGLGrpahicsEngine/Renderer.cpp
@@ -1,12 +1,24 @@
 #include "Renderer.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 {
+	glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	projection = glm::perspective(glm::radians(45.0f), (float)1920 / (float)1080, 0.1f, 100.0f);
+
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"uniform mat4 model;"
+		"uniform mat4 view;"
+		"uniform mat4 projection;"
 		"void main()\n"
 		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
 		"}\0";
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
@@ -18,11 +30,14 @@ void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 	const char* vertexShaderSourceColor = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"layout (location = 1) in vec3 acolor;\n"
+		"uniform mat4 model;"
+		"uniform mat4 view;"
+		"uniform mat4 projection;"
 		"out vec3 acolor_out;"
 		"void main()\n"
 		"{\n"
 		"	acolor_out = acolor;\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"   gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
 		"}\0";
 	const char* fragmentShaderSourceColor = "#version 330 core\n"
 		"in vec3 acolor_out;"
@@ -43,8 +58,6 @@ void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
 	// Color Shaders
 	unsigned int vertexShaderColor = glCreateShader(GL_VERTEX_SHADER);
@@ -57,8 +70,30 @@ void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 	glAttachShader(shaderProgramColor, vertexShaderColor);
 	glAttachShader(shaderProgramColor, fragmentShaderColor);
 	glLinkProgram(shaderProgramColor);
+
+	glUseProgram(shaderProgramColor);
+	unsigned int modelLocColor = glGetUniformLocation(shaderProgramColor, "model");
+	unsigned int viewLocColor = glGetUniformLocation(shaderProgramColor, "view");
+	unsigned int projLocColor = glGetUniformLocation(shaderProgramColor, "projection");
+	glUseProgram(shaderProgram);
+	unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+	unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+	unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
+
+	glUseProgram(shaderProgramColor);
+	glUniformMatrix4fv(modelLocColor, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(viewLocColor , 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLocColor, 1, GL_FALSE, glm::value_ptr(projection));
+
+	glUseProgram(shaderProgram);
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 	glDeleteShader(vertexShaderColor);
 	glDeleteShader(fragmentShaderColor);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	std::vector<float> VertexData, VertexDataColor;
 	for (const auto& [ID, OBJ] : RenderObjects.ObjectStorage)
@@ -89,7 +124,7 @@ void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 		}
 	}
 
-	std::vector<int> IndexData, IndexDataColor;
+	std::vector<int> IndexData, IndexDataColor;	
 	for (const auto& [ID, OBJ] : RenderObjects.ObjectStorage)
 	{
 		if (OBJ->Colors)
@@ -149,7 +184,7 @@ void RenderLoopSetup(LoadedObjects &RenderObjects, GLFWwindow* window)
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, IndexData.size(), GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 	glUseProgram(shaderProgramColor);
 	glBindVertexArray(VAOColor);
